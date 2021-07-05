a00af16f3c26085b1b5ee49ad28921dda9bfcd0d
raguselvaraj <mailmeragu92@gmail.com>
2021-07-05 19:35:51 +0530

    Done & Check

---
+++ src/com/pro/JavaPro.java
@@ -0,0 +1,38 @@
+package com.pro;
+
+import java.util.Scanner;
+
+public class JavaPro {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter the number");
+		Scanner sc=new Scanner(System.in);
+		
+		int nt = sc.nextInt();
+		
+		int a,i=0,j=0;
+		
+		a=nt;
+		
+		while (a>0) {
+			
+			i=a%10;
+			j=(j*10)+i;
+			a=a/10;
+			
+		}
+		System.out.println("The reversed number is::::"+j);
+		if (nt==j) {
+			System.out.println("Its a palindrome");
+			
+			
+		} else {
+
+			System.out.println("No its not a palindrome"); 
+		}
+		
+		
+	}
+
+}
